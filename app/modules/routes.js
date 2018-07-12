11e7d1511de461a4a814876928c6f5c311b651dd
Sam-Guliker <samguliker@hotmail.com>
2018-07-12 15:10:04 +0200

    update

--- app/modules/routes.js
+++ app/modules/routes.js
@@ -11,8 +11,8 @@ const page = {
 
 export const routes = {
     init() {
-        app.get('/', () => hyper(document.body) `${page.introduction}`);
+        app.get('https://sam-guliker.github.io/webdesign/app/index.html', () => hyper(document.body) `${page.introduction}`);
         app.get('/principles', () => hyper(document.body) `${page.principles}`);
-        app.navigate('/');
+        app.navigate('https://sam-guliker.github.io/webdesign/app/index.html');
     }
 }
