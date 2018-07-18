2dc65dcf2ae6bce1fb8e3ba13ce3e452cd772f1d
Sam-Guliker <samguliker@hotmail.com>
2018-07-18 11:05:31 +0200

    working with dist, minify js

--- dist/introduction.js
+++
@@ -1,5 +0,0 @@
-export const introduction = {
-    init: (
-        console.log('hi')
-    )
-}
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const historyFallback = require('connect-history-api-fallback');
-
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+const historyFallback = require('connect-history-api-fallback')
+const useref = require('gulp-useref')
+const uglify = require('gulp-uglify')
+const gulpIf = require('gulp-if')
 
 gulp.task('sass', function () {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
@@ -22,6 +24,14 @@ gulp.task('browserSync', function () {
       ]
     },
   })
+})
+
+gulp.task('html', function () {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', minifyCss()))
+    .pipe(gulp.dest('dist'));
 })
 
 gulp.task('default', ['browserSync', 'sass'], function () {
